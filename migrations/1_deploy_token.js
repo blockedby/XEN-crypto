f2c1a1721f851014f04864c1f9212efb9bd8a3ef
Alex Green <kasnis12@gmail.com>
2022-11-17 01:44:34 +0300

    deleted truffle

--- migrations/1_deploy_token.js
+++
@@ -1,8 +0,0 @@
-const XENCrypto = artifacts.require("XENCrypto");
-const Math = artifacts.require("Math");
-
-module.exports = async function (deployer) {
-  await deployer.deploy(Math);
-  await deployer.link(Math, XENCrypto);
-  await deployer.deploy(XENCrypto);
-};
